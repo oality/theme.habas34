46936ff16d0cb56529afee3f00bb1b350637b5f9
Benoit Suttor <bsuttor@imio.be>
2019-02-26 16:28:36 +0100

    Add upgrade step to remove banner

--- src/theme/habas34/upgrades.py
+++ src/theme/habas34/upgrades.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from plone import api
 from plone.app.upgrade.utils import loadMigrationProfile
 
 
@@ -6,4 +7,11 @@ def reload_gs_profile(context):
     loadMigrationProfile(
         context,
         'profile-theme.habas34:default',
+    )
+
+
+def uninstall_banner(context):
+    portal_setup = api.portal.get_tool('portal_setup')
+    portal_setup.runAllImportStepsFromProfile(
+        'profile-collective.behavior.banner:uninstall'
     )
